35bb0ecca0c29d4af47582dc996ddea0147a510b
Kauane Vieira <kauaneviieira@gmail.com>
2023-04-26 17:36:44 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/projeto05-design-system/' //Sempre será o nome do repositório do github
     }
 
     return config
